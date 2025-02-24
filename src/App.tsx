ca95ec3aa46a8c883accf4f3e7ea2cb5977b7a9a
sw-lee-dev <lsw6212@naver.com>
2025-02-24 12:50:54 +0900

    2025.02.24 12:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Component, { CalssComponent, FunctionalComponent } from './component/Component';
 import './App.css';
+import CurlyBraces from './component/CurlyBraces';
 
 function App() {
   return (
     <div>
-      
+      {/* <CalssComponent />
+      <FunctionalComponent />
+      <Component /> */}
+      <CurlyBraces />
     </div>
   );
 }
